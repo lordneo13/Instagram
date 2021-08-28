4d8eda1591c9e50d1670cc7e1acd958b53bdf99b
lord neo <74961995+lordneo13@users.noreply.github.com>
2021-08-28 19:08:16 +0530

    Update Post.js

--- src/Post.js
+++ src/Post.js
@@ -3,29 +3,20 @@ import React,{useState} from "react";
 import {Modal} from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import "./Post.css";
 
 export default function Post(props){
     const index = props.index;
     const item = props.item;
-    async function get_quote(){
-      const resp = await fetch("https://api.quotable.io/random");
-      const data = await resp.json();
-      setQuote(data.content);
-      setAuthor(data.author);
-    }
+    const list = props.list;
+    const uname = item.uname;
+
     const [show,setShow] = useState(false);
-    const [quote,setQuote] = useState("");
-    const [author,setAuthor] = useState("");
-    const handleShow = () => {
-      get_quote();
-      setShow(true);
-    }
+    const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
 
     return(
     <>
-    <div className="Post">
+    <div className="Post" style={{marginTop:"50px"}}>
     <div className="user" style={{float:"left"}}>
     <img src={item.avatar} style={{
       width: 30,
@@ -33,7 +24,7 @@ export default function Post(props){
       border: "none",
       borderRadius: "50px",
     }} onClick={handleShow}/>
-    <label><b>{item.uname}</b></label>
+    <label><b>{uname}</b></label>
     </div>
     <LazyLoadImage key={index} src={item.src} alt="img" width="100%" effect="blur"/>
     </div>
@@ -45,11 +36,17 @@ export default function Post(props){
             margin: 10,
             border: "none",
             borderRadius: "50px",
-            }}/><lable><b>{item.uname}</b></lable>
+            }}/><lable><b>{uname}</b></lable>
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <b>"<i>{quote}</i>" - {author}.</b>
+        <div>
+          {
+            list ? list.map((item,index) => {
+              return (item.uname === uname && item.uname !== "User") ? <LazyLoadImage key={index} src={item.src} alt="img" width="100%" effect="blur"/> : ""
+            }) : ""
+          }
+        </div>
       </Modal.Body>
     </Modal>
     </>
